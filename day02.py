ad834769ab6720ed54eb861c362349feea392c15
Parker L <leonard.parker5@gmail.com>
2023-12-01 15:27:38 -0500

    Day 02 template

---
+++ day02.py
@@ -0,0 +1,14 @@
+#######################
+# Advent of Code 2023 #
+# DAY 2               #
+#######################
+
+##########
+# PART 1 #
+##########
+file = open('Inputs/day02input.txt', 'r')
+
+##########
+# PART 2 #
+##########
+file = open('Inputs/day02input.txt', 'r')
