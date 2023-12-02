703245147a8791d99eed23a27b92161c6b07f61c
Parker L <leonard.parker5@gmail.com>
2023-12-02 10:33:42 -0500

    Simplified day02 solution

--- day02.py
+++ day02.py
@@ -6,22 +6,17 @@
 ##########
 # PART 1 #
 ##########
-MAX_RED, MAX_GREEN, MAX_BLUE = 12, 13, 14
 file = open('Inputs/day02input.txt', 'r')
 ID_sum = 0
 
 for line in file:
-    reds, greens, blues, valid = 0, 0, 0, True
+    max_red, max_green, max_blue= 0, 0, 0
     games = line.split()
     for i in range(len(games)):
-        if "red" in games[i]: reds += int(games[i-1])
-        if "green" in games[i]: greens += int(games[i-1])
-        if "blue" in games[i]: blues += int(games[i-1])
-        if ";" in games[i] or i == (len(games)-1):
-            if  (reds <= MAX_RED and greens <= MAX_GREEN and blues <= MAX_BLUE): reds, greens, blues = 0, 0, 0
-            if (reds > MAX_RED or greens > MAX_GREEN or blues > MAX_BLUE): valid = False
-    if valid: ID_sum += int(games[1].split(":")[0])
-
+        if "red" in games[i] and int(games[i-1]) > max_red: max_red = int(games[i-1])
+        if "green" in games[i] and int(games[i-1]) > max_green: max_green = int(games[i-1])
+        if "blue" in games[i] and int(games[i-1]) > max_blue: max_blue = int(games[i-1])
+    if (max_red <= 12 and max_green <= 13 and max_blue <= 14): ID_sum += int(games[1].split(":")[0])
 print(ID_sum)
 
 ##########
@@ -35,7 +30,7 @@ for line in file:
     games = line.split()
     for i in range(len(games)):
         if "red" in games[i] and int(games[i-1]) > max_red: max_red = int(games[i-1])
-        if "green" in games[i]and int(games[i-1]) > max_green: max_green = int(games[i-1])
-        if "blue" in games[i]and int(games[i-1]) > max_blue: max_blue = int(games[i-1])
+        if "green" in games[i] and int(games[i-1]) > max_green: max_green = int(games[i-1])
+        if "blue" in games[i] and int(games[i-1]) > max_blue: max_blue = int(games[i-1])
     power_sum += max_red * max_green * max_blue
 print(power_sum)
